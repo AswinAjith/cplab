6d35e8ddd5f2661df274b6342777daa478553e79
dftd <141352334+dftd@users.noreply.github.com>
2023-08-04 03:49:28 +0530

    Update bubblesort.c

--- bubblesort.c
+++ bubblesort.c
@@ -7,8 +7,8 @@ int arr[n];
 printf("Enter elements:");
 for(i=0;i<n;i++)
 scanf("%d",&arr[i]);
-for(i=0;i<n-1;i++){
-   for(j=0;j<n-i-1;j++){
+for(i=0;i<n;i++){
+   for(j=0;j<n-1;j++){
     if(arr[j]>arr[j+1]){
         temp=arr[j];
         arr[j]=arr[j+1];
