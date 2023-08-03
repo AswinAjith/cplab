a419f458758843f52caeefe059edb495e18d1474
dftd <141352334+dftd@users.noreply.github.com>
2023-08-04 04:08:54 +0530

    Create searchandupdate.c

---
+++ searchandupdate.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main(){
+int n,i,search;
+printf("Enter number of elements in list");
+scanf("%d",&n);
+int arr[n+1];
+printf("Enter the elements in list:\n");
+for(i=0;i<n;i++){
+    scanf("%d",&arr[i]);
+}
+printf("Enter the number to check:\n");
+scanf("%d",&search);
+for (i=0;i<n;i++)
+  {
+    if(arr[i]==search)   {
+      printf("%d is present at location %d.\n", search, i+1);
+      break;
+    }
+    else{
+        arr[n]=search;
+        printf("Number not present. The array after adding elements:");
+        for(i=0;i<n;i++)
+        printf("%d",arr[i]);
+    }
+  }
+return 0;
+}
+
+
+
+    
