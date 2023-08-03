88ea4c24637d1ed0a1210c3576b687396af6da62
dftd <141352334+dftd@users.noreply.github.com>
2023-08-04 04:07:45 +0530

    Create secondlargest.c

---
+++ secondlargest.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() {
+int n,i,j,temp=0;
+printf("Enter number of elements in array:");
+scanf("%d",&n);
+int arr[n];
+printf("Enter elements:");
+for(i=0;i<n;i++)
+scanf("%d",&arr[i]);
+for(i=0;i<n;i++){
+   for(j=0;j<n-1;j++){
+    if(arr[j]>arr[j+1]){
+        temp=arr[j];
+        arr[j]=arr[j+1];
+        arr[j+1]=temp;
+    }
+    }
+}
+printf("The second largest number is:%d",arr[n-2]);
+return 0;
+}
