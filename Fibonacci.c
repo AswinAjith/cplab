e04628666bb7c0955f2f5c8cda5672f37708b129
dftd <141352334+dftd@users.noreply.github.com>
2023-08-03 23:04:25 +0530

    Create Fibonacci.c

---
+++ Fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main(){
+int i,t,n;
+int t1=0, t2=1,t3=0;
+printf("Enter number of terms needed for fibonacci:");
+scanf("%d",&n);
+int arr[n];
+arr[0]=t1;
+arr[1]=t2;
+for(i=2;i<n;i++){
+    t3=t1+t2;
+    arr[i]=t3;
+    t1=t2;
+    t2=t3;
+}
+printf("The fibonacci series upto %d terms :",n);
+for(i=0;i<n;i++)
+printf("%d",arr[i]);
+return 0;
+}
+
+    
